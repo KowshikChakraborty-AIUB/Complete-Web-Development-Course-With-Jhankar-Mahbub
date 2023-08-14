f8d0043ab6b1824e45f51c47dfdc01c68b41d94d
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-08-14 21:37:04 +0600

    26-3 Add event listener and compare email password

---
+++ Milestone 5/Module 26/Baap-Er-Bank/JS/login.js
@@ -0,0 +1,18 @@
+//step-1: adding click event handler in submit button
+document.getElementById('btn-submit').addEventListener('click', function(){
+    // step-2: get the email id inside the email input field
+    const emailField = document.getElementById('user-email');
+    const emails = emailField.value;
+
+    // step-3: get the password inside the password field
+    const passwordField = document.getElementById('user-password');
+    const passwords = passwordField.value;
+
+    // step-4: varify email & password [note: do not use this technique of varifying user/password! it's not the ideal way!]
+    if(emails === 'kowshik@gmail.com' && passwords === 'kowshik1234'){
+        console.log('valid user');
+    }else{
+        console.log('invalid user');
+    }
+})
+
