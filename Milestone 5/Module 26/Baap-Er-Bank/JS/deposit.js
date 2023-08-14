273637a076ae10a9da0b3e4c5e551af65363de0a
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-08-14 23:45:36 +0600

    26-7 Update deposit and balance and handle string add

--- Milestone 5/Module 26/Baap-Er-Bank/JS/deposit.js
+++ Milestone 5/Module 26/Baap-Er-Bank/JS/deposit.js
@@ -2,10 +2,29 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
     // step-2: get the deposit amount from the user-deposit input field
     const userDeposit = document.getElementById('user-deposit');
-    const depositAmount = userDeposit.value;
+    const newDepositAmountString = userDeposit.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
-    // step-3: get the current deposit total amount
+
+    // step-3: get the previous deposit total amount
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositAmountString = depositTotalElement.innerText;
+    const previousDepositAmount = parseFloat(previousDepositAmountString);
+
+    // step-4: add new + previous amount to set the total deposit
+    depositTotalElement.innerText = newDepositAmount;
+    const currentDepositTotal = previousDepositAmount + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // step-5: get the previous balance amount
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceAmountString = balanceTotalElement.innerText;
+    const previousBalanceAmount = parseFloat(previousBalanceAmountString);
+
+    // step-6: update the balance after deposit amount
+    const currentBalanceTotal = previousBalanceAmount + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    // step-7: clear the deposit field
+    userDeposit.value = '';
 })
