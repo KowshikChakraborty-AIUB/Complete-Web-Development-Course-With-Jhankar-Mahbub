38a18b64a3d739ee3dbf88c3f99c162350b0ec2a
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-08-15 21:56:22 +0600

    27-7 (advanced) get and set element value using a function

--- Milestone 5/Module 27/Functional-Baap-Er-Bank-JS-Code-Using-Function/JS/deposit.js
+++ Milestone 5/Module 27/Functional-Baap-Er-Bank-JS-Code-Using-Function/JS/deposit.js
@@ -1,17 +1,28 @@
-function getInputFieldValueById(inputId){
-    const inputField = document.getElementById(inputId);
-    const inputFieldValueString = inputField.value;
-    const inputFieldValue = parseFloat(inputFieldValueString);
-    return inputFieldValue;
-}
-
-function getDisplayElementValueById(elementId){
-    const element = document.getElementById(elementId);
-    const elementValueString = element.innerText;
-    const elementValue = parseFloat(elementValueString);
-    return elementValue;
-}
-
 document.getElementById('btn-deposit').addEventListener('click', function(){
+    /* 1. get the input field by id 
+       2. get the input field value
+       3. convert string value to number   
+    */
+
     const newDepositAmount = getInputFieldValueById('user-deposit');
+    
+    // 4. get the previous deposit amount by id
+    const previousDepositAmount = getDisplayElementValueById('deposit-total');
+    
+    // 5. calculate new total deposit
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+
+    // 6. set the new total deposit value
+    setNewDisplayElementValueById('deposit-total', newDepositTotal)
+
+    // 7. get the previous balance amount by id
+    const previousBalanceAmount = getDisplayElementValueById('balance-total')
+
+    // 8. calculate new balance after adding deposit
+    const newBalanceTotal = previousBalanceAmount + newDepositAmount;
+
+    // 9. set the new total balance
+    setNewDisplayElementValueById('balance-total', newBalanceTotal)
+
+
 })
---
+++ Milestone 5/Module 27/Functional-Baap-Er-Bank-JS-Code-Using-Function/JS/utilities.js
@@ -0,0 +1,19 @@
+function getInputFieldValueById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getDisplayElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const elementValue = parseFloat(elementValueString);
+    return elementValue;
+}
+
+function setNewDisplayElementValueById(elementId, newElementTotal){
+    const element = document.getElementById(elementId);
+    element.innerText = newElementTotal;
+}
