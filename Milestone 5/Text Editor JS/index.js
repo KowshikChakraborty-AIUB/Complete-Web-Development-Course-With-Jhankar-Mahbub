3dfc89a27101a0ff32ee4d1cfc80c810ab8e7a97
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-08-17 02:36:04 +0600

    created index.js file and fixed some styling

---
+++ Milestone 5/Text Editor JS/index.js
@@ -0,0 +1,7 @@
+// document.getElementById('make-bold').addEventListener('click', function () {
+//     const textArea = document.getElementById('text-area');
+//     // const textAreavalue = textArea.innerText;
+//     const bold = document.createElement('b');
+//     bold.innerText = textArea;
+//     textArea.appendChild(bold);
+// })
