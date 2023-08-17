cba831fa8aca33c516d3020fc31609ab530b0727
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-08-17 21:43:15 +0600

    28-3 calculate Triangle and Rectangle Area

--- Milestone 5/Module 28/geometry-genius/scripts/area-calculate.js
+++ Milestone 5/Module 28/geometry-genius/scripts/area-calculate.js
@@ -3,11 +3,36 @@ function caculateAreaTriangle(){
     const baseField = document.getElementById('triangle-b');
     const baseValueString = baseField.value;
     const baseValue = parseFloat(baseValueString);
-    console.log(baseValue);
 
     // get triangle height value
     const heightField = document.getElementById('triangle-h');
     const heightValueString = heightField.value;
     const heightValue = parseFloat(heightValueString);
-    console.log(heightValue);
+
+    // calculate triangle area
+    const area = 0.5 * baseValue * heightValue;
+    console.log(area);
+
+    // display the area
+    const displayArea = document.getElementById('triangle-area');
+    displayArea.innerText = area;
+}
+
+function caculateAreaRectangle(){
+    // get rectangle width
+    const widthField = document.getElementById('rectangle-w');
+    const widthValueString = widthField.value;
+    const widthValue = parseFloat(widthValueString);
+
+    // get rectangle length
+    const lengthField = document.getElementById('rectangle-l');
+    const lengthValueString = lengthField.value;
+    const lengthValue = parseFloat(lengthValueString); 
+
+    // calculate area
+    const area = widthValue * lengthValue;
+
+    // display area
+    const displayArea = document.getElementById('rectangle-area');
+    displayArea.innerText = area;
 }
