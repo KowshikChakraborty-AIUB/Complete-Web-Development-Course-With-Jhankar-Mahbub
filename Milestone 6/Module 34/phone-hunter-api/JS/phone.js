d915f79bd4b36babc0a7882d04ed411e6b5cbf81
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-08-27 20:24:51 +0600

    34-2 Display phones dynamically on each card

--- Milestone 6/Module 34/phone-hunter-api/JS/phone.js
+++ Milestone 6/Module 34/phone-hunter-api/JS/phone.js
@@ -1,8 +1,30 @@
 const loadPhone = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const phoneData = await res.json();
-    console.log(phoneData.data);
+    displayPhone(phoneData.data)
+}
 
+
+const displayPhone = phoneData => {
+    const phoneListContainer = document.getElementById('phone-list-container');
+    phoneData.forEach(phone => {
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure class="px-10 pt-10">
+            <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        
+        `
+        phoneListContainer.appendChild(phoneCard);
+    })
 }
 
 loadPhone();
