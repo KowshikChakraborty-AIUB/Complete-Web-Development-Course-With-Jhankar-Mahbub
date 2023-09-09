fe35c494cedbce1ce343813e1cef468017e82c71
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-09-09 22:01:47 +0600

    40-4 Toggle state based on click and conditional rending

--- Milestone 7/Module 40/react-world-tour/src/Components/Country/Country.jsx
+++ Milestone 7/Module 40/react-world-tour/src/Components/Country/Country.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import './Country.css';
 const Country = ({country}) => {
     const {name, flags, population, area} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
+
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
             <img src={flags?.png} alt="" />
             <p>Area: {area} km<sup>2</sup></p>
             <p>Population: {population}</p>
+            <button onClick={handleVisited}>{visited?'Visited':'Going'}</button>
+            {
+                visited?'I visited this counrty':'I want to visit this country!'
+            }
         </div>
     );
 };
