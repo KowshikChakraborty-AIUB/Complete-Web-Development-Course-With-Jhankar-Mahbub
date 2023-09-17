96f9c159606db3b6ecb823fecc6cdf7fd7e152b5
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-09-17 21:56:01 +0600

    44-2 Responsive Navbar using react and tailwind

--- Milestone 8/Module 44/price-options/src/App.jsx
+++ Milestone 8/Module 44/price-options/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+//import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <>
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
       <h1 className='text-7xl bg-orange-700'>Vite + React</h1>
     </>
   )
