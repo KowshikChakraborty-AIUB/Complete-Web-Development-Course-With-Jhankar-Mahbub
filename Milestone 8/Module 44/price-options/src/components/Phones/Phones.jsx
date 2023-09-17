5f62f84763ecfc1a2119625dc55c28ec85085a8b
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-09-18 01:01:25 +0600

    44-8 Explore React Awesome components

--- Milestone 8/Module 44/price-options/src/components/Phones/Phones.jsx
+++ Milestone 8/Module 44/price-options/src/components/Phones/Phones.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { Audio, Grid } from 'react-loader-spinner'
 import { useState } from "react";
 
 const Phones = () => {
 
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
         .then(data => {
@@ -18,11 +20,39 @@ const Phones = () => {
                 return obj
             })
             setPhones(phoneFakeData);
+            setLoading(false);
         })
 
 
     return (
+
         <div>
+
+            {
+                loading && <div>
+                    <Audio
+                        height="80"
+                        width="80"
+                        radius="9"
+                        color='green'
+                        ariaLabel='three-dots-loading'
+                        wrapperStyle
+                        wrapperClass
+                    />
+                    <Grid
+                        height="80"
+                        width="80"
+                        color="#4fa94d"
+                        ariaLabel="grid-loading"
+                        radius="12.5"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                    />
+                </div>
+            }
+
+
             <BarChart width={1200} height={400} data={phones}>
                 <Bar dataKey={'price'} fill="green"></Bar>
                 <XAxis dataKey={'name'}></XAxis>
