a0150414c7116f043a279231f001f03e971ebfe4
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-09-21 20:52:05 +0600

    47-2 (Recap) Set Nested Route and Navbar for CareerHub

--- Milestone 8/Module 47/react-career-hub/src/main.jsx
+++ Milestone 8/Module 47/react-career-hub/src/main.jsx
@@ -5,15 +5,22 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Root from './Components/Root/Root';
 import Home from './Components/Home/Home';
+import AppliedJobs from './Components/AppliedJobs/AppliedJobs';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
-        path: "",
+        path: "/",
         element: <Home></Home>
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>
       }
     ]
   },
