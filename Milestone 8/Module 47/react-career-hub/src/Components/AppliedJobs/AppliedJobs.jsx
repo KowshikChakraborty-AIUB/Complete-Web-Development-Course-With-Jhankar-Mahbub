c39316bc9b6ce2a3f899023c6bc5e123c54d48d6
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-09-22 20:56:45 +0600

    47-9 Implement Job filter and deploy to netlify and surge

--- Milestone 8/Module 47/react-career-hub/src/Components/AppliedJobs/AppliedJobs.jsx
+++ Milestone 8/Module 47/react-career-hub/src/Components/AppliedJobs/AppliedJobs.jsx
@@ -4,8 +4,21 @@ import { useLoaderData } from "react-router-dom";
 const AppliedJobs = () => {
 
     const [jobsApplied, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
 
     const jobs = useLoaderData();
+
+    const handleFilterJobs = filter => {
+        if (filter === 'all') {
+            setDisplayJobs(jobsApplied);
+        } else if (filter === 'remote') {
+            const remoteJobs = jobsApplied.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs);
+        } else if (filter === 'onsite') {
+            const onsiteJobs = jobsApplied.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onsiteJobs);
+        }
+    }
 
     useEffect(() => {
         const storedJobIds = getStoredJobApplication();
@@ -21,14 +34,23 @@ const AppliedJobs = () => {
                 }
             }
             setAppliedJobs(appliedJobs);
+            setDisplayJobs(appliedJobs);
         }
-    }, [])
+    }, [jobs])
 
     return (
         <div>
             <h2>Jobs I Applied : {jobsApplied.length}</h2>
+            <details className="dropdown mb-32">
+                <summary className="m-1 btn">Filter</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <li onClick={() => handleFilterJobs('all')}><a>All</a></li>
+                    <li onClick={() => handleFilterJobs('remote')}><a>Remote</a></li>
+                    <li onClick={() => handleFilterJobs('onsite')}><a>Onsite</a></li>
+                </ul>
+            </details>
             {
-                jobsApplied.map((job, idx) => <li key={job.id}><span>{idx+1} {job.job_title} <br />Company: {job.company_name}</span></li>)
+                displayJobs.map((job, idx) => <li key={job.id}><span>{idx + 1} {job.job_title} <br />Company: {job.company_name}</span></li>)
             }
         </div>
     );
