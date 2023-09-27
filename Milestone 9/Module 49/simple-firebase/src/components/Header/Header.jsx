f8e6d85177b0eed81f80aa75a1b1cc93d472c985
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-09-27 23:31:58 +0600

    49-2 Create firebase project and Install Firebase SDK

---
+++ Milestone 9/Module 49/simple-firebase/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <p>this is header</p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ Milestone 9/Module 49/simple-firebase/src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcZQcGuYeJ9BjCEdUYHR5ZF8Oi6YO1jNw",
+  authDomain: "simple-firebase-9288d.firebaseapp.com",
+  projectId: "simple-firebase-9288d",
+  storageBucket: "simple-firebase-9288d.appspot.com",
+  messagingSenderId: "364784785631",
+  appId: "1:364784785631:web:4b9355e28defe228ed716a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
