19b2f90dbe1f25930abd9e51e9ce542629d83e8a
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-09-27 23:52:41 +0600

    49-3 Initialize firebase auth and set Sign in with google

--- Milestone 9/Module 49/simple-firebase/src/components/Home/Home.jsx
+++ Milestone 9/Module 49/simple-firebase/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
         <div>
-            <p>This is home</p>
+            <Link to={'/'}>
+                <p>This is home</p>
+            </Link>
+            <Link to={'/login'}>Login</Link>
         </div>
     );
 };
