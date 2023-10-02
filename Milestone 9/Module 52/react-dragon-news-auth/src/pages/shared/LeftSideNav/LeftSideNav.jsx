ed623286aa87a61a7be1f0aa349387a80c662e17
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-03 01:42:50 +0600

    52-9 Left side navigation and module summary

--- Milestone 9/Module 52/react-dragon-news-auth/src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ Milestone 9/Module 52/react-dragon-news-auth/src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+
+    }, [])
+
+
     return (
         <div>
-            <h2 className="text-3xl">Left Side Nav</h2>
+            <h2 className="text-3xl">All Categories</h2>
+            {
+                categories.map(category => <Link key={category.id} className="block ml-4 text-sm font-semibold my-8" to={`/category/${category.id}`}>{category.name}</Link>)
+            }
         </div>
     );
 };
