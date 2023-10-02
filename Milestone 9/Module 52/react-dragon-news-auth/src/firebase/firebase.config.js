5690b6761eecf349f7506495c0b5106fed8a2bb3
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-02 23:14:10 +0600

    52-3 Project Overview and Project setup

---
+++ Milestone 9/Module 52/react-dragon-news-auth/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKv39cuyJjmyo1Y2AjN699kJiZc64vzsA",
+  authDomain: "react-dragon-news-auth-5761b.firebaseapp.com",
+  projectId: "react-dragon-news-auth-5761b",
+  storageBucket: "react-dragon-news-auth-5761b.appspot.com",
+  messagingSenderId: "366873680183",
+  appId: "1:366873680183:web:2429288efdae802dfce2b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
