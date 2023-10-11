a109ad665ee8f5cd00c9bf5b7df19f08eea79f58
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-12 01:41:12 +0600

    55-7 Load Single Item By Id And create Update route

--- Milestone 10/Module 55/simple-crud-client/src/components/Users.jsx
+++ Milestone 10/Module 55/simple-crud-client/src/components/Users.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
 
@@ -12,24 +12,28 @@ const Users = () => {
         fetch(`http://localhost:5000/users/${_id}`, {
             method: 'delete'
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data.deletedCount > 0){
-                alert('deleted successfully!')
-                const remaining = users.filter(user => user._id !== _id);
-                setUsers(remaining);
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    alert('deleted successfully!')
+                    const remaining = users.filter(user => user._id !== _id);
+                    setUsers(remaining);
+                }
+            })
     }
 
 
     return (
         <div>
             {
-                users.map(user => <p key={user._id}>{user.name}: {user.email} 
-                
-                    <button onClick={()=>handleDelete(user._id)}>Delete</button>
+                users.map(user => <p key={user._id}>{user.name}: {user.email}
+
+                    <Link to={`/update/${user._id}`}>
+                        <button>Update</button>
+                    </Link>
+
+                    <button onClick={() => handleDelete(user._id)}>Delete</button>
 
                 </p>)
             }
