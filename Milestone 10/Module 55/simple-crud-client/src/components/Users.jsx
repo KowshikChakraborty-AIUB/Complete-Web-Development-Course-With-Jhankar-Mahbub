1b8a154368457bfb54c40d4762e7edb05a94acfa
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-11 23:47:58 +0600

    55-5 Delete A User From the Database using UI click button

--- Milestone 10/Module 55/simple-crud-client/src/components/Users.jsx
+++ Milestone 10/Module 55/simple-crud-client/src/components/Users.jsx
@@ -4,11 +4,29 @@ const Users = () => {
 
     const users = useLoaderData();
 
+    const handleDelete = (_id) => {
+        console.log('delete', _id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'delete'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.deletedCount > 0){
+                alert('deleted successfully!')
+            }
+        })
+    }
+
 
     return (
         <div>
             {
-                users.map(user => <p key={user._id}>{user.name}: {user.email}</p>)
+                users.map(user => <p key={user._id}>{user.name}: {user.email} 
+                
+                    <button onClick={()=>handleDelete(user._id)}>Delete</button>
+
+                </p>)
             }
         </div>
     );
