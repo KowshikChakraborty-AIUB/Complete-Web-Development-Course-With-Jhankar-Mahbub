5075d4ab9e953ac5d490adf1766cef7173fc8086
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-11 22:39:16 +0600

    55-3 Create client side post and Insert single user data

--- Milestone 10/Module 55/simple-crud-client/src/App.jsx
+++ Milestone 10/Module 55/simple-crud-client/src/App.jsx
@@ -2,9 +2,34 @@ import './App.css'
 
 function App() {
 
+  const handleAddUser = (e) => {
+      e.preventDefault()
+      const form = e.target;
+      const name = form.name.value;
+      const email = form.email.value;
+      const user = {name, email};
+      console.log(user);
+      e.target.reset();
+
+      fetch('http://localhost:5000/users', {
+        method: 'post',
+        headers: {
+          'content-type' : 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='name' />
+        <input type="email" name='email' placeholder='email' />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
