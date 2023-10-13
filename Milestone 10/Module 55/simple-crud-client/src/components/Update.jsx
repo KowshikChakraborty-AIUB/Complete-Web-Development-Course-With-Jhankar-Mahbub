247e87e1ba3b152f95d3aa9c21686ae2d37d2cbc
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-13 19:58:44 +0600

    55-8 Explore Put And Patch And Update User To Database

--- Milestone 10/Module 55/simple-crud-client/src/components/Update.jsx
+++ Milestone 10/Module 55/simple-crud-client/src/components/Update.jsx
@@ -10,6 +10,23 @@ const Update = () => {
         const name = form.name.value;
         const email = form.email.value;
         console.log(name, email);
+        const updatedUser = {name, email}
+
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: 'put',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount>0){
+                alert('user updated successfully!')
+            }
+        })
     }
 
 
