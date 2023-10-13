d836d5ee4abd8aeef3f9d07de0dabf5acab034a8
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-13 23:04:56 +0600

    56-3 Server Side hide database User and Password using Dotenv config

--- Milestone 10/Module 56/coffee-store-server/index.js
+++ Milestone 10/Module 56/coffee-store-server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 //middleware
 app.use(cors());
@@ -9,7 +10,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.hxgse1v.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hxgse1v.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +30,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
