86454e218abb5885e8e5be12e506c7c4f9293f61
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-13 23:05:28 +0600

    56-3 Server Side hide database User and Password using Dotenv config

--- Milestone 10/Module 56/coffee-store-client/src/components/AddCoffee.jsx
+++ Milestone 10/Module 56/coffee-store-client/src/components/AddCoffee.jsx
@@ -1,7 +1,27 @@
 const AddCoffee = () => {
     return (
         <div>
-            <h2>Add a Coffee</h2>
+            <h2 className="font-bold text-5xl text-center">Add a Coffee</h2>
+            <form>
+                <div className="flex gap-6 mt-20 justify-center items-center">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-bold">Coffee Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" placeholder="Coffee Name" className="input input-bordered" />
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-bold">Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="number" placeholder="Quantity" className="input input-bordered" />
+                        </label>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
