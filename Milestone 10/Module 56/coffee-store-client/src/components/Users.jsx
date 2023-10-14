45d8e49604798ba4b763e94de6cdfe19dc6fb8da
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-15 01:41:39 +0600

    56_5-5 Delete a user from client side and database

--- Milestone 10/Module 56/coffee-store-client/src/components/Users.jsx
+++ Milestone 10/Module 56/coffee-store-client/src/components/Users.jsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Users = () => {
 
     const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
 
+    const handleDelete = (_id) => {
 
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'User has been deleted.',
+                                'success'
+                            )
+
+                            const remaining = users.filter(user => user._id !== _id)
+                            setUsers(remaining);
+                        }
+                    })
+            }
+        })
+    }
 
     return (
         <div>
-            <h2>Users: {loadedUsers.length}</h2>
+            <h2>Users: {users.length}</h2>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
@@ -22,13 +55,15 @@ const Users = () => {
                     </thead>
                     <tbody>
                         {
-                            loadedUsers.map(user =>
+                            users.map(user =>
 
                                 <tr key={user._id}>
                                     <th>{user._id}</th>
                                     <td>{user.email}</td>
                                     <td>{user.createdAt}</td>
-                                    <td></td>
+                                    <td>
+                                        <button onClick={() => handleDelete(user._id)} className="btn bg-red-600 hover:bg-red-600">X</button>
+                                    </td>
                                 </tr>
 
 
