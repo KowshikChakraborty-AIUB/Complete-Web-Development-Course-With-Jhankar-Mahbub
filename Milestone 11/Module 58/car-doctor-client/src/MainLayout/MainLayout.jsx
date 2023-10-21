7ea309cdb265982fd21f261c565fe607dc3628c4
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-21 22:20:12 +0600

    58-2 Set basic Layout for Footer and header

--- Milestone 11/Module 58/car-doctor-client/src/MainLayout/MainLayout.jsx
+++ Milestone 11/Module 58/car-doctor-client/src/MainLayout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/footer";
+import Navbar from "../Pages/Shared/Navbar";
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
