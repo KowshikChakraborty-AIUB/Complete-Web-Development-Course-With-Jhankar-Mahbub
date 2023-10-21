0532e3415aac3a9b0efeb818df3f6dabd1d61249
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-21 22:53:18 +0600

    58-3 Style Header and create simple banner

---
+++ Milestone 11/Module 58/car-doctor-client/src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,39 @@
+import img1 from '../../../assets/1.jpg'
+import img2 from '../../../assets/2.jpg'
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={img1} className="w-full" />
+                <div className="absolute flex justify-end bottom-0 transform -translate-y-1/2 left-5 right-5">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={img2}className="w-full" />
+                <div className="absolute flex justify-end bottom-0 transform -translate-y-1/2 left-5 right-5">
+                    <a href="#slide1" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                <div className="absolute flex justify-end bottom-0 transform -translate-y-1/2 left-5 right-5">
+                    <a href="#slide2" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+                <div className="absolute flex justify-end bottom-0 transform -translate-y-1/2 left-5 right-5">
+                    <a href="#slide3" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
