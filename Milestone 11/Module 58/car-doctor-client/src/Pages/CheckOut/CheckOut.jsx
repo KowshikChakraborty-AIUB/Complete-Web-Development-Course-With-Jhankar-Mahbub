43773a1c9d188c7f191e2630989a45190cbcd687
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-26 00:31:27 +0600

    59-3 Create checkout Form and load single service data

---
+++ Milestone 11/Module 58/car-doctor-client/src/Pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,52 @@
+import { useLoaderData } from "react-router-dom";
+
+const CheckOut = () => {
+
+    const service = useLoaderData();
+    const { _id, title, price } = service;
+
+    return (
+        <div>
+            <h2>Title: {title}</h2>
+            <form className="card-body">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <input type="submit" value={'Confirm Order'} className="btn btn-primary btn-block"></input>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default CheckOut;
