63b75ace651d92668e4451ef39ceac351791dbbb
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-26 01:17:34 +0600

    59-4 Extract booking service Data

--- Milestone 11/Module 58/car-doctor-client/src/Pages/CheckOut/CheckOut.jsx
+++ Milestone 11/Module 58/car-doctor-client/src/Pages/CheckOut/CheckOut.jsx
@@ -1,44 +1,60 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 const CheckOut = () => {
 
     const service = useLoaderData();
-    const { _id, title, price } = service;
+    const { service_id, _id, title, price } = service;
+    const {user} = useContext(AuthContext);
+
+    const handleCheckOutOrder = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const date = form.date.value;
+        const email = user?.email || 'N/A';
+        const dueAmount = form.dueAmount.value;
+
+        const order = {
+            customerName: name,
+            service_id,
+            date,
+            email,
+            dueAmount
+        }
+
+        console.log(order);
+    }
 
     return (
         <div>
-            <h2>Title: {title}</h2>
-            <form className="card-body">
+            <h2 className="text-3xl text-center font-bold">Order Service: {title}</h2>
+            <form onSubmit={handleCheckOutOrder} className="card-body">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text">Name</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input type="text" defaultValue={user?.displayName} name="name" placeholder="name" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text">Date</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
+                        <input type="date" name="date" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input type="email" defaultValue={user?.email} name="email" placeholder="email" className="input input-bordered" readOnly required />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text">Due Amount</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
+                        <input type="text" defaultValue={`$ ` + price} name="dueAmount" placeholder="due amount" className="input input-bordered" readOnly required />
                     </div>
                 </div>
                 <div className="form-control mt-6">
