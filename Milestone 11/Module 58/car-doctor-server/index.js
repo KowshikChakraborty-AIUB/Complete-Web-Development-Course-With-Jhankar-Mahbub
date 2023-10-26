a763f6290ea7bfeb4fd18cf316acd657d6e35dde
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-26 16:54:12 +0600

    59-8 Create Delete API and Delete a service Booking

--- Milestone 11/Module 58/car-doctor-server/index.js
+++ Milestone 11/Module 58/car-doctor-server/index.js
@@ -62,6 +62,17 @@ async function run() {
             res.send(result);
         })
 
+        app.put('/orders/:id', async (req, res) => {
+            const updatedOrders = req.body;
+        })
+
+        app.delete('/orders/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await orderCollections.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
