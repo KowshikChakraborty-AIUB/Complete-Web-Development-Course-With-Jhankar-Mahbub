d7001c096ca85cc9496efd34bf8fc68160069606
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-26 23:50:39 +0600

    60-3 Access token refresh token and create simple access token

--- Milestone 11/Module 58/car-doctor-server/index.js
+++ Milestone 11/Module 58/car-doctor-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -30,6 +31,15 @@ async function run() {
         const serviceCollections = client.db('carDoctorDB').collection('services')
         const orderCollections = client.db('carDoctorDB').collection('orders')
 
+        //auth related api
+        app.post('/jwt', async(req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, 'secret', {expiresIn: '1h'})
+            res.send(token);
+        })
+
+        //services
         app.get('/services', async (req, res) => {
             const cursor = serviceCollections.find();
             const result = await cursor.toArray();
