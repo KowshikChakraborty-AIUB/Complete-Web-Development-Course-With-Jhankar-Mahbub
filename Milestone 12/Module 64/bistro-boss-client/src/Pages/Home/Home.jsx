a31a5f9277669abcb6e1cb290e33668c859d5280
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-10 02:52:03 +0600

    64-6 Show Popular menu items in the home page

--- Milestone 12/Module 64/bistro-boss-client/src/Pages/Home/Home.jsx
+++ Milestone 12/Module 64/bistro-boss-client/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
+import PopularMenu from "./Popular Menu/PopularMenu";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Category></Category>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
