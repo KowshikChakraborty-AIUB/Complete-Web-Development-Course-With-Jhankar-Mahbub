5f1884c73e4e6bedec814399bfdb2db0c2eeabe7
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-10 02:12:57 +0600

    64-4 Use swiper JS to create a category Swiper

---
+++ Milestone 12/Module 64/bistro-boss-client/src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,54 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import sliderImage1 from '../../../assets/home/slide1.jpg'
+import sliderImage2 from '../../../assets/home/slide2.jpg'
+import sliderImage3 from '../../../assets/home/slide3.jpg'
+import sliderImage4 from '../../../assets/home/slide4.jpg'
+import sliderImage5 from '../../../assets/home/slide5.jpg'
+
+const Category = () => {
+    return (
+        <div className='mb-24'>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src={sliderImage1} alt="" />
+                    <h3 className='text-4xl text-white -mt-12 uppercase mx-16'>Salads</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={sliderImage2} alt="" />
+                    <h3 className='text-4xl text-white -mt-12 uppercase mx-16'>Pizzas</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={sliderImage3} alt="" />
+                    <h3 className='text-4xl text-white -mt-12 uppercase mx-16'>Soups</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={sliderImage4} alt="" />
+                    <h3 className='text-4xl text-white -mt-12 uppercase mx-16'>Deserts</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={sliderImage5} alt="" />
+                    <h3 className='text-4xl text-white -mt-12 uppercase mx-16'>Salads</h3>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Category;
