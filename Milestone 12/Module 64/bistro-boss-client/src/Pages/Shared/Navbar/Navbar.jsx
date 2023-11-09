d6a32c8f592188a8ad6917e3a98cb17055101459
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-10 01:45:40 +0600

    64-3 Create simple Top Banner react responsive carousel

--- Milestone 12/Module 64/bistro-boss-client/src/Pages/Shared/Navbar/Navbar.jsx
+++ Milestone 12/Module 64/bistro-boss-client/src/Pages/Shared/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-black bg-opacity-40 text-white max-w-screen-xl">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
