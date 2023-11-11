3ac1466487ba01e595ee23b479251e13f0c18898
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-11 19:03:22 +0600

    65-7 Select right tab based on the category

--- Milestone 12/Module 64/bistro-boss-client/src/Pages/Order/Order/Order.jsx
+++ Milestone 12/Module 64/bistro-boss-client/src/Pages/Order/Order/Order.jsx
@@ -6,11 +6,14 @@ import 'react-tabs/style/react-tabs.css';
 import useMenu from '../../../hooks/useMenu';
 import OrderTabs from '../Order Tabs/OrderTabs';
 import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 const Order = () => {
-    const [tabIndex, setTabIndex] = useState(0);
-    const [menu] = useMenu();
+    const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks'];
     const { category } = useParams();
+    const initialIndex = categories.indexOf(category);
+    const [tabIndex, setTabIndex] = useState(initialIndex);
+    const [menu] = useMenu();
     const dessert = menu.filter(item => item.category === 'dessert')
     const pizza = menu.filter(item => item.category === 'pizza')
     const soup = menu.filter(item => item.category === 'soup')
@@ -18,6 +21,9 @@ const Order = () => {
     const drinks = menu.filter(item => item.category === 'drinks')
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Order Food</title>
+            </Helmet>
             <Cover menuImg={orderCover} title={'Order Food'}></Cover>
             <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)} className={'text-center my-8'}>
                 <TabList>
--- Milestone 12/Module 64/bistro-boss-client/src/Pages/Shared/Navbar/Navbar.jsx
+++ Milestone 12/Module 64/bistro-boss-client/src/Pages/Shared/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <>
             <li><Link to={'/'}>Home</Link></li>
             <li><Link to={'/menu'}>Our Menu</Link></li>
-            <li><Link to={'/orderFood'}>Order Food</Link></li>
+            <li><Link to={'/orderFood/salad'}>Order Food</Link></li>
             {/* <li>
                 <a>Parent</a>
                 <ul className="p-2">
