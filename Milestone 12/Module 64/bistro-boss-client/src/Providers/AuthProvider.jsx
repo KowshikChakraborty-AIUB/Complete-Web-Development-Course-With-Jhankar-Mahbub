d12229741e67c75c3a217fd988590ee706244a0b
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-16 19:16:51 +0600

    68-7 (Recap) Create a JWT token and save it on local storage

--- Milestone 12/Module 64/bistro-boss-client/src/Providers/AuthProvider.jsx
+++ Milestone 12/Module 64/bistro-boss-client/src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -9,6 +10,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true)
@@ -40,6 +42,21 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubsribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+
+            //get token & store client
+            if(currentUser){
+                const userInfo = {email: currentUser.email}
+                axiosPublic.post('/jwt', userInfo)
+                .then(res => {
+                    if(res.data.token){
+                        localStorage.setItem('access_token', res.data.token)
+                    }
+                })
+            }
+            else{
+                //remove token. (if token stored in the client local storage, caching, in memory.)
+                localStorage.removeItem('access_token')
+            }
             setLoading(false);
         })
         return () => { return unsubsribe() }
