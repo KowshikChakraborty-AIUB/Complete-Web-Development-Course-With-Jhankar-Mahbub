5fef138fa626e657196968861c2f8090ec7b99e8
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-16 19:17:31 +0600

    68-7 (Recap) Create a JWT token and save it on local storage

--- Milestone 12/Module 64/bistro-boss-server/index.js
+++ Milestone 12/Module 64/bistro-boss-server/index.js
@@ -35,30 +35,50 @@ async function run() {
     const reviewCollections = client.db('bistroBossDB').collection('reviews');
     const cartCollections = client.db('bistroBossDB').collection('carts');
 
+    //jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      console.log(req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      next();
+    }
+
     //users related api
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyToken, async (req, res) => {
       const result = await userCollections.find().toArray();
       res.send(result);
     })
 
-    app.post('/users', async(req, res) => {
+    app.post('/users', async (req, res) => {
       const user = req.body;
       //insert email if user doesn't exist
       //it can be done in many ways (1. email unique, 2. upsert, 3. simple checking)
-      const query = {email: user.email};
+      const query = { email: user.email };
       const existingUser = await userCollections.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already exists', insertedId: null});
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null });
       }
       const result = await userCollections.insertOne(user);
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
-        $set : {
+        $set: {
           role: 'admin'
         }
       }
@@ -66,39 +86,39 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/users/:id', async(req, res) => {
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await userCollections.deleteOne(query);
       res.send(result);
     })
 
     //menu related api
-    app.get('/menu', async(req, res) => {
-        const result = await menuCollections.find().toArray();
-        res.send(result);
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result);
     })
-    app.get('/reviews', async(req, res) => {
-        const result = await reviewCollections.find().toArray();
-        res.send(result);
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollections.find().toArray();
+      res.send(result);
     })
 
     //carts collections
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', async (req, res) => {
       const email = req.query.email;
-      const query = {email: email};
+      const query = { email: email };
       const result = await cartCollections.find(query).toArray();
       res.send(result);
-  })
-    app.post('/carts', async(req, res) => {
+    })
+    app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollections.insertOne(cartItem);
       res.send(result);
     })
 
-    app.delete('/carts/:id', async(req, res) => {
+    app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollections.deleteOne(query);
       res.send(result);
     })
@@ -115,9 +135,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('boss is eating');
+  res.send('boss is eating');
 })
 
 app.listen(port, () => {
-    console.log(`bistro boss is eating on port ${port}`);
+  console.log(`bistro boss is eating on port ${port}`);
 })
