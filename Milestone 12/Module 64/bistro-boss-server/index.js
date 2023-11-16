dfc03da16ae9fc9348d0a524210646b3ace315ec
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-16 20:07:12 +0600

    68-8 (advanced) verify token and axios interceptor

--- Milestone 12/Module 64/bistro-boss-server/index.js
+++ Milestone 12/Module 64/bistro-boss-server/index.js
@@ -52,7 +52,13 @@ async function run() {
       if (!token) {
         return res.status(401).send({ message: 'unauthorized access' })
       }
-      next();
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
     }
 
     //users related api
