07ad72c845975e85b3bec4ef862e7faa452e47b2
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-16 23:17:55 +0600

    68-9 Logout unauthorized access and check is admin

--- Milestone 12/Module 64/bistro-boss-server/index.js
+++ Milestone 12/Module 64/bistro-boss-server/index.js
@@ -44,14 +44,14 @@ async function run() {
 
     //middlewares
     const verifyToken = (req, res, next) => {
-      console.log(req.headers);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: 'unauthorized access' })
       }
       const token = req.headers.authorization.split(' ')[1];
-      if (!token) {
-        return res.status(401).send({ message: 'unauthorized access' })
-      }
+      //console.log(token);
+      // if (!token) {
+      //   return res.status(401).send({ message: 'unauthorized access' })
+      // }
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
           return res.status(401).send({ message: 'unauthorized access' })
@@ -65,6 +65,20 @@ async function run() {
     app.get('/users', verifyToken, async (req, res) => {
       const result = await userCollections.find().toArray();
       res.send(result);
+    })
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userCollections.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
     })
 
     app.post('/users', async (req, res) => {
