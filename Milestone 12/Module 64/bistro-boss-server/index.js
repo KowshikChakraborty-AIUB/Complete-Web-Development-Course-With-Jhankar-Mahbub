86b00b7bd210fcab9d7de00fda710c66dcd56030
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-17 00:03:23 +0600

    68-10 (advanced) Make user api secure using verify Admin

--- Milestone 12/Module 64/bistro-boss-server/index.js
+++ Milestone 12/Module 64/bistro-boss-server/index.js
@@ -61,8 +61,22 @@ async function run() {
       })
     }
 
+    //use verifyAdmin after verifyToken
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollections.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next();
+
+    }
+
     //users related api
-    app.get('/users', verifyToken, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollections.find().toArray();
       res.send(result);
     })
@@ -94,7 +108,7 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -106,7 +120,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/users/:id', async (req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollections.deleteOne(query);
