63805c89a69cbec077c9515f54b33a8bdfe49afe
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-11-23 01:43:19 +0600

    71-7 (Optional) Display custom bar chart and custom pie chart

--- Milestone 12/Module 64/bistro-boss-client/src/Pages/Dashboard/Admin Home/AdminHome.jsx
+++ Milestone 12/Module 64/bistro-boss-client/src/Pages/Dashboard/Admin Home/AdminHome.jsx
@@ -3,6 +3,10 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaBook, FaDollarSign, FaUsers } from "react-icons/fa6";
 import { MdMenuBook } from "react-icons/md";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, PieChart, Pie, Legend } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const AdminHome = () => {
     const { user } = useAuth();
@@ -15,6 +19,48 @@ const AdminHome = () => {
             return res.data;
         }
     })
+
+    const { data: chartData } = useQuery({
+        queryKey: ['orderStats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/orderStats');
+            return res.data;
+        }
+    })
+
+
+    //custom shape for the bar chart
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
+
+    //custom shape for the pie chart
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+    const pieChartData = chartData.map(data => {
+        return { name: data.category, value: data.revenue };
+    })
+
     return (
         <div>
             <h2 className="text-3xl">
@@ -35,7 +81,7 @@ const AdminHome = () => {
 
                 <div className="stat">
                     <div className="stat-figure text-secondary">
-                        <FaDollarSign className="text-3xl"/>
+                        <FaDollarSign className="text-3xl" />
                     </div>
                     <div className="stat-title">Revenue</div>
                     <div className="stat-value">${stats?.revenue}</div>
@@ -44,7 +90,7 @@ const AdminHome = () => {
 
                 <div className="stat">
                     <div className="stat-figure text-secondary">
-                        <FaUsers className="text-3xl"/>
+                        <FaUsers className="text-3xl" />
                     </div>
                     <div className="stat-title">Users</div>
                     <div className="stat-value">{stats?.users}</div>
@@ -53,7 +99,7 @@ const AdminHome = () => {
 
                 <div className="stat">
                     <div className="stat-figure text-secondary">
-                    <MdMenuBook className="text-3xl"/>
+                        <MdMenuBook className="text-3xl" />
                     </div>
                     <div className="stat-title">Menu</div>
                     <div className="stat-value">{stats?.menuItems}</div>
@@ -62,13 +108,56 @@ const AdminHome = () => {
 
                 <div className="stat">
                     <div className="stat-figure text-secondary">
-                        <FaBook className="text-3xl"/>
+                        <FaBook className="text-3xl" />
                     </div>
                     <div className="stat-title">Orders</div>
                     <div className="stat-value">{stats?.orders}</div>
                     <div className="stat-desc">↘︎ 90 (14%)</div>
                 </div>
 
+            </div>
+            <div className="flex">
+                <div className="w-1/2">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData?.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </div>
+                <div className="w-1/2">
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            data={pieChartData}
+                            cx="50%"
+                            cy="50%"
+                            labelLine={false}
+                            label={renderCustomizedLabel}
+                            outerRadius={80}
+                            fill="#8884d8"
+                            dataKey="value"
+                        >
+                            {pieChartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Legend />
+                    </PieChart>
+                </div>
             </div>
         </div>
     );
